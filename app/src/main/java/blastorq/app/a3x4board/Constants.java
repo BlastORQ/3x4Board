d248b9f17677e67261d2e7529dc0ef8825400b9c
Manzhula David <da411d@gmail.com>
2017-07-17 17:17:35 +0300

    Загрузив все, намагався розібратись..

---
+++ app/src/main/java/blastorq/app/a3x4board/Constants.java
@@ -0,0 +1,13 @@
+package blastorq.app.a3x4board;
+
+public class Constants {
+    public static class KeyCode {
+        public static final int SPACE = 32;
+        public static final int RETURN = 10;
+    }
+
+    public enum KEYS_TYPE {
+        SYMBOLS, EN, UA
+    }
+
+}
